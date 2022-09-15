1778e08b7fb908a1c53063018508e9253caa9cb5
Ohene Afrane <okesebrimpong@gmail.com>
2022-09-15 17:32:45 +0300

    Assignment on modules

---
+++ notebooks/assignment2_modules.py
@@ -0,0 +1,8 @@
+def list_max(list_of_numbers): #a function that finds the maximum value in a list of numbers
+    return max(list_of_numbers)
+
+def average_of_list(list_of_numbers): # A function that calculates the average of list of numbers 
+    return sum(list_of_numbers)/len(list_of_numbers)
+
+def sorted_string(string_to_be_sorted): # A function that sorts a string into alphabetical order.
+    return sorted(string_to_be_sorted)
